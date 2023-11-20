a7e88ef5129e8fd31ee534579931c56618919cd3
Rafael <rafiuxxdxd@gmail.com>
2023-11-20 02:23:58 -0600

    Ventana Articulos y Empleados
    Se hizo la interfaz para las ventanas de Articulos y la de Empleados

--- src/main/java/com/mycompany/aplicacion/Aplicacion.java
+++ src/main/java/com/mycompany/aplicacion/Aplicacion.java
@@ -5,7 +5,7 @@ import gui.VentanaRecargas;
 public class Aplicacion {
 
     public static void main(String[] args) {
-        //Ejecutar Ventana Recargas para pruebas:
+        //Ejecutar Ventanas para pruebas:
 //       VentanaRecargas obVR = new VentanaRecargas();
 //       obVR.setVisible(true);
     }
