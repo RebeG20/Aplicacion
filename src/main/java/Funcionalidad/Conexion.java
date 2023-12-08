d1a92a9ec132ddcbf5f2619ee7ead1bed4154c2f
RebeG20 <151072899+RebeG20@users.noreply.github.com>
2023-12-08 02:53:27 -0600

    Cambio sin importancia

--- src/main/java/Funcionalidad/Conexion.java
+++ src/main/java/Funcionalidad/Conexion.java
@@ -9,8 +9,8 @@ public class Conexion {
     Connection conexion = null;
     String user = "root";
     //Base de datos de Rebeca:
-//    String password="Rebecaga20";
-//    String database="tiendav2";
+    String password="Rebecaga20";
+    String database="tiendav2";
     
 //    //Base de datos de Rebeca:
 //    String password="Rebecaga20";
@@ -29,8 +29,8 @@ public class Conexion {
 //    String database = "tiendav2";
 
 //    Base de datos lupita 
-    String password="rootmaria";
-    String database="tiendav2";
+//    String password="rootmaria";
+//    String database="tiendav2";
 //    Base de datos lupita 
 //    String password="rootmaria";
 //    String database="tienda";
